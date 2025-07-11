9081f5f3b5e311ae68c33bcfc2fed3ed425012e3
thomaskitaba <thomas.kitaba@gmail.com>
2025-07-11 16:28:42 +0300

    control with arrow keys

--- src/mower_robot/mower_robot/teleop.py
+++ src/mower_robot/mower_robot/teleop.py
@@ -8,14 +8,14 @@ class TeleopNode(Node):
     def __init__(self):
         super().__init__('teleop_node')
         self.publisher = self.create_publisher(Twist, '/mower_robot/cmd_vel', 10)
-        self.get_logger().info("Use WASD to move, X to stop, Ctrl+C to quit")
+        self.get_logger().info("Use Arrow keys to move, Spacebar to stop, Ctrl+C to quit")
         self.settings = termios.tcgetattr(sys.stdin)
         self.moveBindings = {
-            'w': (1.0, 0.0),  # Forward
-            's': (-1.0, 0.0), # Backward
-            'a': (0.0, 1.0),  # Left
-            'd': (0.0, -1.0), # Right
-            'x': (0.0, 0.0),  # Stop
+            '\x1b[A': (1.0, 0.0),   # Up arrow: Forward
+            '\x1b[B': (-1.0, 0.0),  # Down arrow: Backward
+            '\x1b[D': (0.0, 1.0),   # Left arrow: Turn left
+            '\x1b[C': (0.0, -1.0),  # Right arrow: Turn right
+            ' ': (0.0, 0.0),        # Spacebar: Stop
         }
         self.speed = 0.5
         self.turn = 1.0
@@ -24,6 +24,9 @@ class TeleopNode(Node):
         tty.setraw(sys.stdin.fileno())
         select.select([sys.stdin], [], [], 0)
         key = sys.stdin.read(1)
+        # Handle arrow keys (escape sequences)
+        if key == '\x1b':  # Escape character
+            key += sys.stdin.read(2)  # Read [A, [B, [C, or [D
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
         return key
 
